9afb799576d1012b45930f0974a47ba1265c14f8
Sven Mengler <s.s.mengler@gmx.net>
2017-11-11 13:33:14 +0100

    added a vector class

---
+++ src/linearAlgebra/Vector.java
@@ -0,0 +1,26 @@
+package linearAlgebra;
+
+/**
+ * this class represents a vector
+ * @author sven
+ */
+public class Vector {
+    
+    // vector size
+    int m;
+    // vector itself
+    double[] vector;
+    
+    /**
+     * initialize 0.0 vector with length m >= 1
+     * @param m 
+     */
+    public Vector(int m) {
+    
+        if (m < 1) 
+            throw new IllegalArgumentException("Vector length must be >= 1!");
+        
+        this.m = m;
+        vector = new double[m];
+    }
+}
